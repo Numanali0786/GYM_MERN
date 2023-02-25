a5cb0de05277f19685f1ebeea3c6b3cf3c472626
Numan Ali <numan4599322ali@gmail.com>
2023-02-25 15:32:53 +0530

    cors

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
 const userRoutes = require("./routes/user");
-import cors from "cors";
+const cors = require("cors");
 
 // express app
 const app = express();
